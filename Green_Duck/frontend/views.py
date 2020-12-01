d1a1e01c2139df888cfe106fde13a5a79be5a92c
louissailor <61705564+louissailor@users.noreply.github.com>
2020-12-01 09:59:26 +0300

    Test

--- Green_Duck/frontend/views.py
+++ Green_Duck/frontend/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+atext = ["hello" , "hi", "igi"]
 def index(request):
-    return HttpResponse("Yigit")
+     return render(request, "index.html" , {
+     "heading": atext[2],
+     "text": atext[1]
+     })
